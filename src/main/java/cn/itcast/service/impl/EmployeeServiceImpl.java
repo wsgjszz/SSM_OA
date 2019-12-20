af44be82f322b8afb9eb53332e7fbd49b3646aee
wsgjszz <wsgjszz@163.com>
2019-12-20 14:40:11 +0800

    开发中

--- src/main/java/cn/itcast/service/impl/EmployeeServiceImpl.java
+++ src/main/java/cn/itcast/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package cn.itcast.service.impl;
 
 import cn.itcast.dao.EmployeeDao;
 import cn.itcast.domain.Employee;
+import cn.itcast.domain.PageBean;
 import cn.itcast.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,26 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDao.findAll();
+    }
+
+    @Override
+    public PageBean<Employee> findByPage(int currentPage, int pageSize) {
+        //创建PageBean对象
+        PageBean<Employee> pageBean = new PageBean<>();
+        //设置当前页码，每页显示记录数
+        pageBean.setPageSize(pageSize);
+        pageBean.setCurrentPage(currentPage);
+        //计算起始页码
+        int start = (currentPage - 1) * pageSize;
+        //查询并设置分页显示的数据集合
+        List<Employee> list = employeeDao.findPageWithDepartName(start, pageSize);
+        pageBean.setList(list);
+        //计算总记录数
+        int totalCount = employeeDao.findTotalCount();
+        //设置总记录数
+        pageBean.setTotalCounts(totalCount);
+        //设置总页数
+        pageBean.setTotalPages(totalCount/pageSize+1);
+        return pageBean;
     }
 }
--- src/main/java/cn/itcast/utils/PageBeanInfo.java
+++ src/main/java/cn/itcast/utils/PageBeanInfo.java
@@ -12,10 +12,12 @@ public class PageBeanInfo {
 
     private PageBean pageBean;
 
-    private int[] pageNums; //当前显示的页码范围
-    private Boolean isFirstNum; //是否是首页
-    private Boolean isEndNum; //是否是尾页
-    private int navigatePages; //连续显示页数
+    private int[] pageNums;      //当前显示的页码范围
+    private Boolean isFirstNum;  //是否是首页
+    private Boolean isEndNum;    //是否是尾页
+    private int navigatePages;   //连续显示页数
+    private int startPage;       //开始页码
+    private int endPage;         //结束页码
 
     public PageBeanInfo(){}
 
@@ -29,26 +31,34 @@ public class PageBeanInfo {
     public void bulidPageNums(PageBean pageBean){
         int currentPage = pageBean.getCurrentPage();
         int totalPages = pageBean.getTotalPages();
-        if (currentPage<=navigatePages){
-            pageNums = new int[navigatePages];
+        pageNums = new int[navigatePages];
+        if (currentPage<=Math.floor(navigatePages/2)+1){
             for (int i = 0; i < navigatePages; i++) {
+                if (i==0) startPage=i+1;
+                if (i==navigatePages-1) endPage=i+1;
                 pageNums[i]=i+1;
             }
-        }else if (currentPage>=totalPages-navigatePages){
+        }else if (currentPage>totalPages-Math.floor(navigatePages/2)-1){
             for (int i = 0; i < navigatePages; i++) {
-                pageNums[i]=totalPages-navigatePages+i;
+                if (i==0) startPage= totalPages-navigatePages+i+1;
+                if (i==navigatePages-1) endPage=totalPages-navigatePages+i+1;
+                pageNums[i]=totalPages-navigatePages+i+1;
             }
         }else {
             if (navigatePages%2==0){
                 //连续显示偶数页码
                 int half = navigatePages / 2; //左边页码
                 for (int i = 0; i < navigatePages; i++) {
+                    if (i==0) startPage=currentPage-half+i;
+                    if (i==navigatePages-1) endPage=currentPage-half+i;
                     pageNums[i]=currentPage-half+i;
                 }
             }else {
                 //连续显示奇数页码
                 int half = navigatePages / 2; //左右的页码
                 for (int i = 0; i < navigatePages; i++) {
+                    if (i==0) startPage=currentPage-half+i;
+                    if (i==navigatePages-1) endPage=currentPage-half+i;
                     pageNums[i]=currentPage-half+i;
                 }
             }
@@ -75,6 +85,8 @@ public class PageBeanInfo {
                 ", isFirstNum=" + isFirstNum +
                 ", isEndNum=" + isEndNum +
                 ", navigatePages=" + navigatePages +
+                ", startPage=" + startPage +
+                ", endPage=" + endPage +
                 '}';
     }
 }
